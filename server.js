0cc34bdf0c73e6922a321b2f63d6ccf417908e64
Chandra Kiran Achanta <chandrakiran.av@gmail.com>
2019-07-09 03:44:10 -0500

    bug fix

--- server.js
+++ server.js
@@ -38,16 +38,21 @@ app.post('/upload', (req,res)=>{
 })
 
 //Serve static assets in production
-if(process.env.NODE_ENV === 'production') {
-    //Static folder
-    app.use(express.static('client/build'))
+// if(process.env.NODE_ENV === 'production') {
+//     //Static folder
+//     app.use(express.static('client/build'))
 
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+//     app.get('*', (req,res)=>{
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
-app.use(express.static(path.join(__dirname, "client/build")))
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+
+// app.use(express.static(path.join(__dirname, "client/build")))
 
 
 
