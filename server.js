45bc5638f440c2317d3e190e85c3e00a922b7781
Chandra Kiran Achanta <chandrakiran.av@gmail.com>
2019-07-09 10:21:29 -0500

    deployment issues fix

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const path = require('path');
 //configuring middleware for body-parser
 app.use(express.json({extended:false}));
 app.use(fileUpload());
-// app.use(express.static(path.join(__dirname, "client/build")))
+app.use(express.static(path.join(__dirname, "client/build")))
 //connect Database
 connectDB();
 
+app.get('/', (req,res) =>{
+    res.send("API is running");
+})
 
 //Route Definitions
 app.use('/api/user', require('./routes/user'));
@@ -19,6 +22,7 @@ app.use('/api/posts', require('./routes/posts'));
 app.use('/api/tweets', require('./routes/tweets'));
 app.use('/api/events', require('./routes/events'));
 app.use('/api/weather', require('./routes/weather'));
+
 
 //File Upload end point
 
@@ -37,29 +41,11 @@ app.post('/upload', (req,res)=>{
     });
 })
 
-//Serve static assets in production
-// if(process.env.NODE_ENV === 'production') {
-//     //Static folder
-//     app.use(express.static('client/build'))
 
-//     app.get('*', (req,res)=>{
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
-
+//Unhandled Route
 app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.status(404).json({ msg: "Unhandled Route, 404 Not found!"});
 })
-
-
-// app.use(express.static(path.join(__dirname, "client/build")))
-
-
-
-// //Unhandled Route
-// app.get('*', (req,res)=>{
-//     res.status(404).json({ msg: "Unhandled Route, 404 Not found!"});
-// })
 
 //Starting server
 const PORT = process.env.PORT || 5000;
