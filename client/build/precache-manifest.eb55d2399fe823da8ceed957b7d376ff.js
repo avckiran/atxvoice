b9a0f819148fe047c75e1df0dae83398ad0a50ba
Ubuntu <ubuntu@ip-172-31-83-37.ec2.internal>
2020-07-18 16:05:11 +0000

    blah blah

---
+++ client/build/precache-manifest.eb55d2399fe823da8ceed957b7d376ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19176e9f544ed680969171d8bf3e2729",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581b8b4b7c6303b08c4a",
+    "url": "/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "581b8b4b7c6303b08c4a",
+    "url": "/static/js/2.4792569f.chunk.js"
+  },
+  {
+    "revision": "1befc57b24befa599f93",
+    "url": "/static/js/main.1849c4cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4029c2b0590a87b3195b09b574bbc746",
+    "url": "/static/media/atx-voice-full-logo.4029c2b0.png"
+  }
+]);
